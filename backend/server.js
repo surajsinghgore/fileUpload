6d2977c516f8af911fb1838355b244157aa248b5
suraj singh <surajthakurrs45@gmail.com>
2023-02-19 14:37:49 +0530

    verision 1.0.1

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 const express=require('express')
 const app=express();
-const port=process.env.Production||8080;
+const port=process.env.Production;
 const ConnectionDB =require('./middleware/DbConnection');
 const SingleFile=require('./router/SingleFile');
 const MultiFile=require('./router/MultiFile');
 const allFiles=require('./router/GetFile');
+const path = require("path");
 require('dotenv').config()
+
+
 app.use(ConnectionDB)
 app.use(express.static(__dirname+'/public'))
+app.use(express.static(path.join(__dirname, "../clients/build")));
 app.get("/",(req,res)=>{
 res.status(200).send("dd")
 })
+
 // single file [GET,POST] request
 app.use("/api",SingleFile);
 app.use("/api",MultiFile);
